602034e7814a4bb4c742836a6949d85b9d708baa
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-08-11 16:49:36 +0100

    Added footer

--- index.php
+++ index.php
@@ -53,6 +53,11 @@
 </div><!-- /.container -->
 
 <!-- footer start-->
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+    </div>
+</footer>
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
