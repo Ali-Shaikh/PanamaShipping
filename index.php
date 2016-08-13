13f1b3173c54eeda29780c5b0549674d31434d0b
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-08-13 16:03:24 +0100

    Refactored login page

--- index.php
+++ index.php
@@ -24,14 +24,14 @@ include "includes/header.php"
             ?>
         </div>
     </div>
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/login.js"></script>
+<!---->
+<!--<!-- Bootstrap core JavaScript-->
+<!--================================================== -->-->
+<!--<!-- Placed at the end of the document so the pages load faster -->-->
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+<!--<script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>-->
+<!--<script src="js/bootstrap.min.js"></script>-->
+<!--<script src="js/login.js"></script>-->
 
 </body>
 </html>
