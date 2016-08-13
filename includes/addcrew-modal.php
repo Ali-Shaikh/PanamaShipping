ba81096e9f584854cf1ca4097cd6ea9918b3d168
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-08-13 18:32:20 +0100

    Added second Form for adding Crew Member

--- includes/addcrew-modal.php
+++ includes/addcrew-modal.php
@@ -78,120 +78,136 @@
                     </div>
                     <div class="form-group">
                         <label class="control-label">Date Joined</label>
-                        <input type="text" class="form-control" id="formInput23" placeholder="Date Started Working at the Company">
+                        <input type="text" class="form-control" id="formInput23"
+                               placeholder="Date Started Working at the Company">
                     </div>
                 </form>
             </div>
             <div class="modal-body step-2" data-step="2">
                 <form role="form">
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="select">
-                        Select a Choice
-                    </label>
-                    <select class="select form-control" id="select" name="select">
-                        <option value="First Choice">
-                            First Choice
-                        </option>
-                        <option value="Second Choice">
-                            Second Choice
-                        </option>
-                        <option value="Third Choice">
-                            Third Choice
-                        </option>
-                    </select>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="date">
-                        Date
-                    </label>
-                    <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="date1">
-                        Date
-                    </label>
-                    <input class="form-control" id="date1" name="date1" placeholder="MM/DD/YYYY" type="text"/>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="select1">
-                        Select a Choice
-                    </label>
-                    <select class="select form-control" id="select1" name="select1">
-                        <option value="First Choice">
-                            First Choice
-                        </option>
-                        <option value="Second Choice">
-                            Second Choice
-                        </option>
-                        <option value="Third Choice">
-                            Third Choice
-                        </option>
-                    </select>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="date2">
-                        Date
-                    </label>
-                    <input class="form-control" id="date2" name="date2" placeholder="MM/DD/YYYY" type="text"/>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="date3">
-                        Date
-                    </label>
-                    <input class="form-control" id="date3" name="date3" placeholder="MM/DD/YYYY" type="text"/>
-                </div>
-                <div class="form-group col-md-6">
-                    <label class="control-label " for="select2">
-                        Select a Choice
-                    </label>
-                    <select class="select form-control" id="select2" name="select2">
-                        <option value="First Choice">
-                            First Choice
-                        </option>
-                        <option value="Second Choice">
-                            Second Choice
-                        </option>
-                        <option value="Third Choice">
-                            Third Choice
-                        </option>
-                    </select>
-                </div>
-                <div class="form-group col-md-6">
-                    <label class="control-label " for="name">
-                        Name
-                    </label>
-                    <input class="form-control" id="name" name="name" type="text"/>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="name1">
-                        Name
-                    </label>
-                    <input class="form-control" id="name1" name="name1" type="text"/>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="date4">
-                        Date
-                    </label>
-                    <input class="form-control" id="date4" name="date4" placeholder="MM/DD/YYYY" type="text"/>
-                </div>
-                <div class="form-group col-md-4">
-                    <label class="control-label " for="date5">
-                        Date
-                    </label>
-                    <input class="form-control" id="date5" name="date5" placeholder="MM/DD/YYYY" type="text"/>
-                </div>
-                <div class="form-group col-md-6">
-                    <label class="control-label " for="name2">
-                        Name
-                    </label>
-                    <input class="form-control" id="name2" name="name2" type="text"/>
-                </div>
-                <div class="form-group col-md-6">
-                    <label class="control-label " for="name3">
-                        Name
-                    </label>
-                    <input class="form-control" id="name3" name="name3" type="text"/>
-                </div>
+                    <div class="form-group col-md-6">
+                        <label class="control-label " for="select">
+                            Rank
+                        </label>
+                        <select class="select form-control" id="select" name="select">
+                            <option value="First Choice">
+                                Captain / Master
+                            </option>
+                            <option value="Second Choice">
+                                Chief Engineer
+                            </option>
+                            <option value="Third Choice">
+                                Oiler
+                            </option>
+                            <option value="Fourth Choice">
+                                Steward
+                            </option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label class="control-label " for="date">
+                            Start Date
+                        </label>
+                        <input class="form-control" id="date" name="date" placeholder="DD/MM/YYYY" type="text"/>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label class="control-label " for="date1">
+                            End Date
+                        </label>
+                        <input class="form-control" id="date1" name="date1" placeholder="DD/MM/YYYY" type="text"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label class="control-label " for="select1">
+                            Company Name
+                        </label>
+                        <select class="select form-control" id="select1" name="select1">
+                            <option value="First Choice">
+                                Prosperity Bay Shipping
+                            </option>
+                            <option value="Second Choice">
+                                Dredging International NV
+                            </option>
+                            <option value="Third Choice">
+                                Messers. Vamva Shipping
+                            </option>
+                            <option value="Fourth Choice">
+                                Acquis Business Systems
+                            </option>
+                            <option value="Fifth Choice">
+                                Other
+                            </option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label class="control-label " for="date2">
+                            Start Date
+                        </label>
+                        <input class="form-control" id="date2" name="date2" placeholder="DD/MM/YYYY" type="text"/>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label class="control-label " for="date3">
+                            End Date
+                        </label>
+                        <input class="form-control" id="date3" name="date3" placeholder="DD/MM/YYYY" type="text"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label class="control-label " for="select2">
+                            Ship
+                        </label>
+                        <select class="select form-control" id="select2" name="select2">
+                            <option value="First Choice">
+                                M/V Ourania Luck
+                            </option>
+                            <option value="Second Choice">
+                                M/V Anna Smile
+                            </option>
+                            <option value="Third Choice">
+                                M/V Captain George-L
+                            </option>
+                            <option value="Fourth Choice">
+                                M/V Ourania Hope
+                            </option>
+                            <option value="Fifth Choice">
+                                M/V Aeriko
+                            </option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label class="control-label " for="name">
+                            Salary
+                        </label>
+                        <input class="form-control" id="name" name="name" type="text"/>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label class="control-label " for="name1">
+                            Company Name
+                        </label>
+                        <input class="form-control" id="name1" name="name1" type="text"/>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label class="control-label " for="date4">
+                            Start Date
+                        </label>
+                        <input class="form-control" id="date4" name="date4" placeholder="DD/MM/YYYY" type="text"/>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label class="control-label " for="date5">
+                            End Date
+                        </label>
+                        <input class="form-control" id="date5" name="date5" placeholder="DD/MM/YYYY" type="text"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label class="control-label " for="name2">
+                            Ship Name
+                        </label>
+                        <input class="form-control" id="name2" name="name2" type="text"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label class="control-label " for="name3">
+                            Salary
+                        </label>
+                        <input class="form-control" id="name3" name="name3" type="text"/>
+                    </div>
                 </form>
             </div>
             <div class="modal-body step-3" data-step="3">
